0929f3ca173a0c34c773981da1a087ed075aff62
Qman11010101 <30486982+Qman11010101@users.noreply.github.com>
2024-12-02 23:17:19 +0900

    perfection

--- generate_chunithm.py
+++ generate_chunithm.py
@@ -96,8 +96,11 @@ for const_block in constlist:
         imgsrc = hashlib.md5((title + artist).encode('utf-8')).hexdigest()
         html_img_str = f'<img src="{IMAGE_URL_BASE}{imgsrc}.webp" class="{diff.lower()}" loading="lazy">'
         html_titleblock_str = f'<div class="titleblock">{html_span(title)}</div>'
+        unknown_str = ""
+        if song["unknown"]:
+            unknown_str = '<div class="unknown-marker"></div>'
         items.append(html_items(
-            html_img_str + html_titleblock_str,
+            html_img_str + html_titleblock_str + unknown_str,
             diff == "ULT"
         ))
 
